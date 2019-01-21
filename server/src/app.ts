1c3c7c8ebfeaf68959af0a91d5727c645316b9db
sam <sam.a.mez@gmail.com>
2019-01-21 12:38:07 -0800

    Added postgres database connection

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,4 +1,16 @@
-const main = () => {
-    console.log('Hello there typscript');
+import {config} from 'dotenv';
+import {Client} from 'pg';
+
+config();
+const client = new Client();
+
+const main = async () => {
+    await client.connect();
+
+    client.query('SELECT NOW()', (err, res) => {
+        console.log(err, res);
+        client.end();
+    });
 };
+
 main();
