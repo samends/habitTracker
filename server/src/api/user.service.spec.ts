5e55e7cb1e5aaa86d59a260da300009bf7787bce
sam <sam.a.mez@gmail.com>
2019-02-16 15:38:14 -0800

    Added delete user tests

--- server/src/api/user.service.spec.ts
+++ server/src/api/user.service.spec.ts
@@ -208,4 +208,174 @@ describe('userService', () => {
                 });
         });
     });
+
+    describe('updating a password', () => {
+        const mockUser = new Users();
+        beforeEach(() => {
+            mockRepository = {
+                find: jasmine.createSpy().and.returnValue([dbUser]),
+                createQueryBuilder: jasmine.createSpy().and.returnValue({
+                    update: jasmine.createSpy().and.returnValue({
+                        set: jasmine.createSpy().and.returnValue({
+                            where: jasmine.createSpy().and.returnValue({
+                                execute: jasmine.createSpy(),
+                            }),
+                        }),
+                    }),
+                }),
+            };
+            mockDb = {
+                manager: jasmine.createSpyObj('manager', ['save']),
+                getRepository: (...params) => {
+                    return mockRepository;
+                },
+            };
+            mockConnection = new MockConnection(mockDb);
+            mockUser.username = user.username;
+            mockUser.password = hash;
+        });
+        it('should call create query builder', () => {
+            userService = new UserService(mockConnection, hashService),
+                userService.updatePassword('1234', 'merp').then(() => {
+                    expect(mockRepository.createQueryBuilder).toHaveBeenCalled();
+                });
+        });
+        it('should call update', () => {
+            userService = new UserService(mockConnection, hashService),
+                userService.updatePassword('1234', 'merp').then(() => {
+                    expect(mockRepository.createQueryBuilder().update).toHaveBeenCalledWith(Users);
+                });
+        });
+        it('should call set', () => {
+            userService = new UserService(mockConnection, hashService),
+                userService.updatePassword('1234', 'merp').then(() => {
+                    expect(mockRepository.createQueryBuilder().update().set).toHaveBeenCalledWith({ password: hash });
+                });
+        });
+        it('should call where', () => {
+            userService = new UserService(mockConnection, hashService),
+                userService.updatePassword('1234', 'merp').then(() => {
+                    expect(mockRepository.createQueryBuilder().update().set().where)
+                        .toHaveBeenCalledWith('id = :id', { id: '1234' });
+                });
+        });
+        it('should call execute', () => {
+            userService = new UserService(mockConnection, hashService),
+                userService.updatePassword('1234', 'merp').then(() => {
+                    expect(mockRepository.createQueryBuilder().update().set().where().execute)
+                        .toHaveBeenCalled();
+                });
+        });
+        it('should call find', () => {
+            userService = new UserService(mockConnection, hashService),
+                userService.updatePassword('1234', 'merp').then(() => {
+                    expect(mockRepository.find).toHaveBeenCalledWith({id: '1234'});
+                });
+        });
+        it('should return the correct value', () => {
+            userService = new UserService(mockConnection, hashService),
+                userService.updatePassword('1234', 'merp').then((res) => {
+                    expect(res).toEqual(dbUser);
+                });
+        });
+    });
+
+    describe('updating a password throws an error', () => {
+        const mockUser = new Users();
+        beforeEach(() => {
+            mockRepository = {
+                find: jasmine.createSpy().and.returnValue([dbUser]),
+                createQueryBuilder: () => {
+                    throw new Error('test error');
+                },
+            };
+            mockDb = {
+                manager: jasmine.createSpyObj('manager', ['save']),
+                getRepository: (...params) => {
+                    return mockRepository;
+                },
+            };
+            mockConnection = new MockConnection(mockDb);
+            mockUser.username = user.username;
+            mockUser.password = hash;
+        });
+        it('should call create query builder', () => {
+            userService = new UserService(mockConnection, hashService),
+                userService.updatePassword('1234', 'merp').catch((error) => {
+                    expect(error).toEqual(new Error('test error'));
+                });
+        });
+    });
+
+    describe('delete a user', () => {
+        const mockUser = new Users();
+        beforeEach(() => {
+            mockRepository = {
+                find: jasmine.createSpy().and.returnValue([dbUser]),
+                createQueryBuilder: jasmine.createSpy().and.returnValue({
+                    delete: jasmine.createSpy().and.returnValue({
+                        from: jasmine.createSpy().and.returnValue({
+                            where: jasmine.createSpy().and.returnValue({
+                                execute: jasmine.createSpy(),
+                            }),
+                        }),
+                    }),
+                }),
+            };
+            mockDb = {
+                manager: jasmine.createSpyObj('manager', ['save']),
+                getRepository: (...params) => {
+                    return mockRepository;
+                },
+            };
+            mockConnection = new MockConnection(mockDb);
+            mockUser.username = user.username;
+            mockUser.password = hash;
+        });
+        it('should call find', () => {
+            userService = new UserService(mockConnection, hashService),
+                userService.deleteUser('12345').then(() => {
+                    expect(mockRepository.find).toHaveBeenCalledWith({id: '12345'});
+                });
+        });
+        it('should call create query builder', () => {
+            userService = new UserService(mockConnection, hashService),
+                userService.deleteUser('12345').then(() => {
+                    expect(mockRepository.createQueryBuilder).toHaveBeenCalled();
+                });
+        });
+        it('should call delete', () => {
+            userService = new UserService(mockConnection, hashService),
+                userService.deleteUser('12345').then(() => {
+                    expect(mockRepository.createQueryBuilder().delete).toHaveBeenCalled();
+                });
+        });
+        it('should call from', () => {
+            userService = new UserService(mockConnection, hashService),
+                userService.deleteUser('12345').then(() => {
+                    expect(mockRepository.createQueryBuilder().delete().from)
+                        .toHaveBeenCalledWith(Users);
+                });
+        });
+        it('should call where', () => {
+            userService = new UserService(mockConnection, hashService),
+                userService.deleteUser('12345').then(() => {
+                    expect(mockRepository.createQueryBuilder().delete().from().where)
+                        .toHaveBeenCalledWith('id = :id', {id: '12345'});
+                });
+        });
+        it('should call execute', () => {
+            userService = new UserService(mockConnection, hashService),
+                userService.deleteUser('12345').then(() => {
+                    expect(mockRepository.createQueryBuilder().delete().from().where().execute)
+                        .toHaveBeenCalled();
+                });
+        });
+        it('should return the correct value', () => {
+            userService = new UserService(mockConnection, hashService),
+                userService.deleteUser('12345').then((res) => {
+                    expect(res).toEqual(dbUser);
+                });
+        });
+    });
 });
